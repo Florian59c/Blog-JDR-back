9081343af37f8df079cd5f922b6edc3a47f0753a
Florian Cagnon <florian.cagnon@live.fr>
2025-02-02 17:51:25 +0100

    feat: back improvement

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { GetEmailDto } from './dto/get-email.dto';
 import { FindUserByIdDto } from './dto/find-user-by-id.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
@@ -33,20 +32,5 @@ export class UserController {
   @Post('updatePassword')
   updatePassword(@Body() updatePasswordDto: UpdatePasswordDto) {
     return this.userService.updatePassword(updatePasswordDto);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
   }
 }
