02b4201bc8ec57c4682400af2a8ef590e953e09f
Florian Cagnon <florian.cagnon@live.fr>
2025-03-07 15:54:40 +0100

    feat: send mail upgrade

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, MinLength } from "class-validator";
 
 export class LoginDto {
-      @IsEmail({}, { message: "Votre Email doit avoir un format du type : exemple@gmail.com. " })
+      @IsEmail({}, { message: "L\'adresse mail doit avoir un format du type : exemple@gmail.com. " })
       email: string;
 
       @MinLength(8, { message: 'Votre mot de passe contient au moins 8 caractères.' })
--- src/mailer/dto/forgot-password.dto.ts
+++ src/mailer/dto/forgot-password.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail } from "class-validator";
 
 export class ForgotPasswordDto {
-    @IsEmail({}, { message: "Votre Email doit avoir un format du type : exemple@gmail.com." })
+    @IsEmail({}, { message: "L\'adresse mail doit avoir un format du type : exemple@gmail.com." })
     email: string;
 }
