1a96acf6d0ade82a5c39b2e0e9fe54c552600871
Florian Cagnon <florian.cagnon@live.fr>
2025-04-28 14:30:07 +0200

    feat: login optimization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login-auth.dto';
 import { UserService } from 'src/user/user.service';
@@ -7,6 +7,7 @@ import { Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import { UserRole } from './interfaces/auth.interface';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { ResponseMessage } from './interfaces/response.interface';
 
 @Injectable()
 export class AuthService {
@@ -15,25 +16,32 @@ export class AuthService {
     private readonly userService: UserService
   ) { }
 
-  async login(loginDto: LoginDto, res: Response): Promise<string> {
+  async login(loginDto: LoginDto, res: Response): Promise<ResponseMessage> {
     try {
       const { email, password } = loginDto;
       const findedUserByMail = await this.userService.findUserByMail({ email });
+
       if (!findedUserByMail || !(await bcrypt.compare(password, findedUserByMail.password))) {
-        return "L'adresse mail ou le mot de passe est incorrect";
+        throw new UnauthorizedException("L'adresse mail ou le mot de passe est incorrect");
       }
+
       const payload = { sub: findedUserByMail.id, role: findedUserByMail.role }; // Inclut l'ID et le rôle
       const jwt = this.jwtService.sign(payload);
+
       res.cookie('auth-token', jwt, {
         httpOnly: true, // Empêche l'accès au cookie via JavaScript côté client
         secure: process.env.NODE_ENV === 'production', // Active uniquement en HTTPS en production
         maxAge: 60 * 60 * 1000, // Durée de vie : 1 heure
         sameSite: 'strict', // Empêche l'envoi des cookies sur des requêtes cross-site
       });
-      return 'ok';
+
+      return { message: 'Connexion réussie' };
     } catch (error) {
       console.error(error);
-      return 'Une erreur est survenue lors de la connexion';
+      if (error instanceof UnauthorizedException) {
+        throw error; // on relance, Nest gère le 401
+      }
+      throw new InternalServerErrorException('Une erreur est survenue lors de la connexion');
     }
   }
 
