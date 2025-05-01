38ad1017a782e3783e7d7a29bccc863263a41a9a
Florian Cagnon <florian.cagnon@live.fr>
2025-05-01 17:00:57 +0200

    feat: createJdr optimization

--- src/jdr/jdr.controller.ts
+++ src/jdr/jdr.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { JdrService } from './jdr.service';
 import { CreateJdrDto } from './dto/create-jdr.dto';
 import { GetsortedJdrDto } from './dto/get-sorted-jdr.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('jdr')
 export class JdrController {
   constructor(private readonly jdrService: JdrService) { }
 
   @Post('createJdr')
+  @UseGuards(AdminGuard)
   createJdr(@Body() createJdrDto: CreateJdrDto) {
     return this.jdrService.createJdr(createJdrDto);
   }
--- src/jdr/jdr.service.ts
+++ src/jdr/jdr.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateJdrDto } from './dto/create-jdr.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Jdr } from './entities/jdr.entity';
 import { JdrList } from 'src/jdr_list/entities/jdr_list.entity';
 import { GetsortedJdrDto } from './dto/get-sorted-jdr.dto';
+import { ResponseMessage } from 'src/interfaces/response.interface';
 
 @Injectable()
 export class JdrService {
@@ -15,22 +16,25 @@ export class JdrService {
     private readonly jdrListRepository: Repository<JdrList>,
   ) { }
 
-  async createJdr(createJdrDto: CreateJdrDto): Promise<string> {
+  async createJdr(createJdrDto: CreateJdrDto): Promise<ResponseMessage> {
     const { title, link, is_scenario, jdr_list_id } = createJdrDto;
     try {
       const existTitle = await this.jdrRepository.findOneBy({ title });
       if (existTitle) {
-        return 'Le titre du document existe déjà';
+        throw new BadRequestException('Le titre du document existe déjà');
       }
+
       const existLink = await this.jdrRepository.findOneBy({ link });
       if (existLink) {
-        return 'Le lien du document existe déjà';
+        throw new BadRequestException('Le lien du document existe déjà');
       }
+
       // Récupération de l'entité JdrList
       const jdrList = await this.jdrListRepository.findOneBy({ id: jdr_list_id });
       if (!jdrList) {
-        return "Le nom du JDR spécifiée n'existe pas dans la liste.";
+        throw new BadRequestException('Le nom du JDR spécifié n\'existe pas dans la liste');
       }
+
       const newJdr = this.jdrRepository.create({
         title,
         link,
@@ -38,28 +42,32 @@ export class JdrService {
         jdr_list: jdrList
       });
       await this.jdrRepository.save(newJdr);
-      return "ok";
+
+      return { message: `L${is_scenario ? 'e scénario' : '\'aide de jeu'} a bien été créé${is_scenario ? '' : 'e'}` };
     } catch (error) {
       console.error(error);
-      return "Un problème est survenu lors de la création d'un JDR";
+      if (error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(
+        'Un problème est survenu lors de la création d\'un JDR',
+      );
     }
   }
 
-  // lorsque jdrName === default, Donne la liste des scénarios si "is_scenario" passé dans le body est vrai. Donne la liste des aide de jeu sinon.
-  // lorsque jdrName !== default, vérifie si la valeur de jdrName correspond à un nom de JDR dans la BDD "jdrList"
+  // lorsque jdrName === default, Donne la liste des scénarios si 'is_scenario' passé dans le body est vrai. Donne la liste des aide de jeu sinon.
+  // lorsque jdrName !== default, vérifie si la valeur de jdrName correspond à un nom de JDR dans la BDD 'jdrList'
   // Si c'est le cas, donne la liste des scénarios ou des aide jeu trié par nom du JDR souhaité
   async getsortedJdr(getsortedJdrDto: GetsortedJdrDto): Promise<Jdr[]> {
     const { is_scenario, jdrName } = getsortedJdrDto;
-    if (jdrName !== "default") {
+    if (jdrName !== 'default') {
       const jdrList = await this.jdrListRepository.findOne({
         where: { name: jdrName },
       });
       if (!jdrList) {
-        throw new Error(`Le nom de la liste de JDR "${jdrName}" n'a pas été trouvé.`);
+        throw new Error(`Le nom de la liste de JDR '${jdrName}' n'a pas été trouvé.`);
       }
     }
     return this.jdrRepository.find({
-      where: jdrName === "default"
+      where: jdrName === 'default'
         ? { is_scenario }
         : { is_scenario, jdr_list: { name: jdrName } },
       order: { date: 'DESC' },
