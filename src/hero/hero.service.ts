280906348474d7332e158e06a3d69c16931f932c
Florian Cagnon <florian.cagnon@live.fr>
2025-05-01 16:44:18 +0200

    feat: createHero optimization

--- src/hero/hero.service.ts
+++ src/hero/hero.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateHeroDto } from './dto/create-hero.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Hero } from './entities/hero.entity';
 import { Repository } from 'typeorm';
+import { ResponseMessage } from 'src/interfaces/response.interface';
 
 @Injectable()
 export class HeroService {
@@ -11,23 +12,29 @@ export class HeroService {
     private readonly heroRepository: Repository<Hero>, // Injecte le Repository TypeORM
   ) { }
 
-  async createHero(createHeroDto: CreateHeroDto): Promise<string> {
+  async createHero(createHeroDto: CreateHeroDto): Promise<ResponseMessage> {
     const { title, link, tag } = createHeroDto;
     try {
       const existTitle = await this.heroRepository.findOneBy({ title })
       if (existTitle) {
-        return 'Le titre du document existe déjà';
+        throw new BadRequestException('Le titre du document existe déjà');
       }
+
       const existLink = await this.heroRepository.findOneBy({ link })
       if (existLink) {
-        return 'Le lien du document existe déjà';
+        throw new BadRequestException('Le lien du document existe déjà');
       }
+
       const newHero = this.heroRepository.create({ title, link, tag }); // Prépare l'utilisateur
-      this.heroRepository.save(newHero); // Insère dans la base
-      return "ok";
+      await this.heroRepository.save(newHero); // Insère dans la base
+
+      return { message: 'L\'histoire dont vous êtes le héros a bien été créée' };
     } catch (error) {
       console.error(error);
-      return "Un problème est survenu lors de la création d'une histoire dont vous êtes le héro";
+      if (error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(
+        'Un problème est survenu lors de la création d’une histoire dont vous êtes le héros.',
+      );
     }
   }
 
