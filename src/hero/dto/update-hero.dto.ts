b5eb2df1984779ffac35a45ef6070d621e3b44d4
Florian Cagnon <florian.cagnon@live.fr>
2025-06-11 14:46:29 +0200

    feat: modify hero request

---
+++ src/hero/dto/update-hero.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class UpdateHeroDto {
+    id: number;
+
+    @IsNotEmpty({ message: 'Le titre est obligatoire. ' })
+    title: string;
+
+    @IsNotEmpty({ message: 'Le lien est obligatoire. ' })
+    link: string;
+
+    @IsString({ message: 'Le tag doit être une chaîne de caractères.' })
+    tag: string;
+}
